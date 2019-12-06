eeb8de73a6fdb2e9ddeefb56212da02e8a872261
minhpham <demonhuter90@gmail.com>
2019-12-06 14:52:24 +0700

    edit post UI prototype

--- admin.js
+++ admin.js
@@ -1,16 +1,53 @@
 var express = require("express")
+var fs  = require("fs")
 var bodyParser = require("body-parser")
 var app = express()
-var sqlUtil = require("../sql")
+var sqlUtil = require("./sql")
+var marked = require("marked")
+var favicon = require('serve-favicon')
+var path = require("path")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static("public"));
 app.set("view engine", "ejs")
+app.use(favicon(path.join(__dirname, "public", "favicon", "favicon.ico")))
+
+//setup marked
+var renderer = new marked.Renderer()
+
+//overriding
+
+renderer.link = (href) => {
+    var id = href.substring(1)
+    return `<a href="${href}" id="${id}"></a>`
+}
+marked.setOptions({
+    renderer: renderer,
+    headerIds: false
+})
 
 app.get("/", async function(req, res) {
     var posts = await sqlUtil.getPosts();
-    res.render("home",{ posts: posts })
+    res.render("admin-home",{ posts: posts })
+})
+
+app.get("/newpost", function(req, res ){
+    res.render("newpost")
+})
+
+app.get("/post/:post", async function(req, res) {
+    var postLink = req.params.post
+    var path = __dirname + "/posts/" + postLink + ".md"
+    var postInfo = await sqlUtil.getThePost(postLink)
+    console.log(postInfo)
+    var data = fs.readFileSync(path, "utf-8")
+    data = data.toString();
+    data = marked(data, (err, result) => {
+        if (err) throw err
+        postInfo.content = result;
+        res.render("editPost", { postInfo: postInfo })
+    })
 })
 
 app.post("/",function(req, res) {
